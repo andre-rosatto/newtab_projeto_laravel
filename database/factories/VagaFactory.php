a5c649491bb5d25c03ff07348b57dbb3fca92fa1
André Rosatto <linus1178@gmail.com>
2024-09-12 16:29:21 -0300

    model e seed

---
+++ database/factories/VagaFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class VagaFactory extends Factory
+{
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array
+	{
+		return [
+			'empresa' => fake()->company(),
+			'titulo' => fake()->jobTitle(),
+			'descricao' => fake()->text(),
+			'localizacao' => fake()->randomElement(['A', 'B', 'C', 'D', 'E', 'F']),
+			'nivel' => fake()->randomElement([1, 2, 3, 4, 5])
+		];
+	}
+}
